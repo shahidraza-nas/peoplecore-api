6172b5fcfb446cea8f15f6234a1dad81e5289244
Shahid Raza <shahid@newagesysit.com>
2025-12-04 16:40:27 +0530

    fix: Update UpdatePageDto and UpdateTemplateDto to use PickType for specific fields

--- src/modules/sql/template/dto/update-template.dto.ts
+++ src/modules/sql/template/dto/update-template.dto.ts
@@ -1,6 +1,13 @@
-import { OmitType, PartialType } from '@nestjs/swagger';
+import { PartialType, PickType } from '@nestjs/swagger';
 import { Template } from '../entities/template.entity';
 
 export class UpdateTemplateDto extends PartialType(
-  OmitType(Template, ['name', 'active'] as const),
+  PickType(Template, [
+    'title',
+    'send_email',
+    'email_subject',
+    'email_body',
+    'send_sms',
+    'sms_body',
+  ] as const),
 ) {}
